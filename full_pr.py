6edcc62990824d37781663f2db5e52a8791ff76e
Lingfan Yu <ly979@nyu.edu>
2018-05-12 19:09:29 +0000

    use vairable instead of while_loop

--- full_pr.py
+++ full_pr.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from util import load_full_graph, elapse
 import time
+import numpy as np
 
 d = 0.85
 n, indices, values = load_full_graph()
@@ -19,29 +20,32 @@ with tf.device('/device:CPU:0'):
     m = tf.sparse_reorder(m)
 
 with tf.device('/device:GPU:0'):
-    p = tf.constant(1.0 / n, shape=[n, 1])
-    delta = tf.constant(1.0)
-    step = tf.constant(0)
-    # terminate when relative delta of norm is less than epsilon
-    cond = lambda step, p, delta : tf.logical_and(step < 100,  delta > 1e-5)
-    sum_p = tf.reduce_sum(p)
-    def body(step, p, delta):
-        last_p = p
-        p = tf.sparse_tensor_dense_matmul(m, p) + (1 - d) / n
-        # set shape for while_loop shape inference
-        p.set_shape([n, 1])
-        delta = tf.norm(p - last_p) / tf.norm(last_p)
-        return [step + 1, p, delta]
+    p = tf.get_variable("pagerank", trainable=False,
+            initializer=tf.constant(1.0 / n, shape=[n, 1]))
+    new_p = tf.sparse_tensor_dense_matmul(m, p) + (1 - d) / n
+    delta = tf.abs(new_p - p) / p
+    # make sure assign happens later
+    with tf.control_dependencies([delta]):
+        assignment = p.assign(new_p)
 
-    res = tf.while_loop(cond, body, loop_vars=[step, p, delta], back_prop=False)
-    step, pr, delta = res
-    sum_pr = tf.reduce_sum(pr)
-
-print("finish dataflow graph construction")
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-
-print("start session run")
 start = time.time()
-print(sess.run([pr, delta, step, sum_p, sum_pr]))
+sess.run(p.initializer)
+i = 0
+max_iter = 100
+while i < max_iter:
+    res = sess.run([delta, assignment])
+    norm = np.sort(np.reshape(res[0], -1))
+    norm = norm[int(n * 0.99)]
+    print("iteration {}:".format(i))
+    print("99 percentile delta is {}".format(norm))
+    # terminate when relative delta of norm is less than epsilon
+    # convergence: 99 percentile < 1e-5
+    if norm < 1e-5:
+        break
+    i += 1
+
 end = time.time()
-elapse(start, end, "session run")
+print(sess.run(p))
+elapse(start, end, "iteration")
+
