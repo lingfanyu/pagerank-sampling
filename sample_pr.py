bd48c83990f4b0cfe20354217faebede265df3f3
Lingfan Yu <ly979@nyu.edu>
2018-05-18 05:29:59 +0000

    sampling version, converges on small graph

--- sample_pr.py
+++ sample_pr.py
@@ -13,73 +13,64 @@ def main(args):
     # global page rank value array
     global_pr = np.array([[1.0 / n_vertex]] * n_vertex, dtype=np.float32)
 
+    with tf.device('/device:GPU:0'):
+        n_sampled = tf.placeholder(tf.int32)
+        local_pr = tf.placeholder(tf.float32)
+        indices = tf.placeholder(tf.int64)
+        values = tf.placeholder(tf.float32)
+        dense_shape = tf.placeholder(tf.int64)
+        m = tf.SparseTensor(indices=indices,
+                            values=values,
+                            dense_shape=dense_shape)
+        new_pr = d * tf.sparse_tensor_dense_matmul(m, local_pr) \
+                + tf.reduce_sum(local_pr) * (1 - d) / tf.to_float(n_sampled)
+        sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
 
+    shuffle = range(args.samples)
     for epoch in range(args.epochs):
         print("epoch {} / {}".format(epoch, args.epochs))
         # shuffle samples
-        shuffle = range(args.samples)
-        random.shuffle(shuffle)
-        datasets = [datasets[i] for i in shuffle]
+        if args.shuffle:
+            random.shuffle(shuffle)
 
-        # pr buffer
+        # reset tensorflow graph
+        # tf.reset_default_graph()
+
         pr_buffer = global_pr.copy()
 
-        # page rank in mini-batch fashion
-        for sample_idx in range(args.samples):
-            # reset tensorflow graph
-            tf.reset_default_graph()
-
-            print("sample {} / {}".format(sample_idx, args.samples))
-
+        for sample_idx in shuffle:
             # sample batches and create dataflow graph
+            sample = datasets[sample_idx]
 
-            v_sampled, indices, values, new2old = datasets[sample_idx]
-            n_sampled = len(v_sampled)
+            # one iteration
+            pr_value = sess.run(new_pr,
+                    feed_dict={local_pr: pr_buffer[sample[0]],
+                               indices: np.array(sample[1], np.int64),
+                               values: sample[2],
+                               dense_shape: np.array([len(sample[0]), len(sample[0])], np.int64),
+                               n_sampled: len(sample[0])})
 
-            # Multiply M with damping factor
-            values = [d * v for v in values]
+            # scatter update local buffer
+            pr_buffer[sample[0]] = pr_value
 
-            # put on CPU first because sparse_reorder does not have a GPU impl...
-            with tf.device('/device:CPU:0'):
-                m = tf.SparseTensor(indices=indices,
-                                    values=values,
-                                    dense_shape=[n_sampled, n_sampled])
-                # sort sparse indices in lexicographical order
-                m = tf.sparse_reorder(m)
+        # endfor sample
 
-            fetch = []
-            with tf.device('/device:GPU:0'):
-                pr = tf.constant(global_pr[v_sampled])
-                new_pr = tf.sparse_tensor_dense_matmul(m, pr) + (1 - d) / n_sampled
-                # make sure assign happens later
-
-            # iterate until subgraph convergence or 100 iterations
-            sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
-            pr_value = sess.run(new_pr)
-
-            # write out
-            for k, v in enumerate(pr_value):
-                pr_buffer[new2old[k]] = v
-
-        # end for sample
         norm = np.abs(pr_buffer - global_pr) / global_pr
         norm = np.sort(np.reshape(norm, -1))
-        print(len(norm))
-        print(int(0.99 * n_vertex))
         norm99 = norm[int(0.99 * n_vertex)]
+        norm50 = norm[int(0.5 * n_vertex)]
+    #    time.sleep(1)
         global_pr = pr_buffer
-        print("norm: {}".format(norm99))
-        time.sleep(1)
+        print("norm: p50 {}, p99 {}".format(norm50, norm99))
+        print(np.sum(global_pr))
+        with open(args.output, "w") as f:
+            for v in global_pr:
+                f.write(str(v[0]) + '\n')
         if norm99 < 1e-5:
             break
 
-    # end for epoch
 
-    print(global_pr)
-    print(np.sum(global_pr))
-    with open(args.output, "w") as f:
-        for v in global_pr:
-            f.write(str(v[0]) + '\n')
+    # endfor epoch
 
 
 
@@ -91,12 +82,14 @@ if __name__ == '__main__':
             help="dataset to use")
     parser.add_argument("--seed", type=int, default=20180512,
             help="random seed")
-    parser.add_argument("--samples", type=int, default=8192,
+    parser.add_argument("--samples", type=int, default=1000,
             help="total number of batches")
-    parser.add_argument("--output", type=str, default="result.txt",
+    parser.add_argument("--output", type=str, default="result_aaa.txt",
             help="output file name")
     parser.add_argument("--epochs", type=int, default=1000,
             help="number of epochs")
+    parser.add_argument("--shuffle", action="store_true",
+            help="whether or not to shuffle")
     args = parser.parse_args()
     print(args)
     random.seed(args.seed)
