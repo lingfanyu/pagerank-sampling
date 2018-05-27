74bf6b969a6713127184317ec52d8b4e770dee3a
Lingfan Yu <ly979@nyu.edu>
2018-05-27 19:34:36 +0000

    count # times sampled for each node

--- test_covering.cc
+++ test_covering.cc
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <unordered_set>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,8 +18,8 @@ public:
 
 using Edges = vector<pair<int, int> >;
 using VSet = unordered_set<int>;
-using ESet = unordered_set<pair<int, int>, pairhash>;
-
+using VCount = unordered_map<int, int>;
+using ECount = unordered_map<pair<int, int>, int, pairhash>;
 
 unordered_set<int> bfs_sampler(int num_vertex, unordered_map<int, vector<int>>& adj, float percent) {
     int n_sample = int(num_vertex * percent);
@@ -30,17 +31,17 @@ unordered_set<int> bfs_sampler(int num_vertex, unordered_map<int, vector<int>>& 
 
     // choose random start point
     // original graph is 1-indexed
-    int start = rand() % num_vertex + 1;
-    res.insert(start);
-    queue.push_back(start);
+    int start_node = rand() % num_vertex + 1;
+    res.insert(start_node);
+    queue.push_back(start_node);
 
     int pos = 0;
     int count = 1;
     while (pos < queue.size() && count < n_sample) {
         int cur = queue[pos++];
         auto& es = adj[cur];
-        for (int dst: es) {
-            if (res.find(dst) == res.end()) {
+        random_shuffle(es.begin(), es.end());
+        for (int dst: es) { if (res.find(dst) == res.end()) {
                 res.insert(dst);
                 queue.push_back(dst);
                 ++count;
@@ -50,7 +51,6 @@ unordered_set<int> bfs_sampler(int num_vertex, unordered_map<int, vector<int>>& 
             }
         }
     }
-
     return move(res);
 }
 
@@ -65,40 +65,53 @@ unordered_set<int> uniform_sampler(int num_vertex, const Edges& edges, float per
 }
 
 
-void update_vertex(VSet& vs, ESet& es, const Edges& edges, const VSet&& v_sampled) {
+void update_vertex(VCount& vs, ECount& es, const Edges& edges, const VSet&& v_sampled) {
     for (int v: v_sampled) {
-        vs.insert(v);
+        if (vs.find(v) == vs.end()) {
+            vs[v] = 0;
+        }
+        ++vs[v];
     }
+    /*
     for (auto& e: edges) {
         if (v_sampled.find(e.first) != v_sampled.end() && v_sampled.find(e.second) != v_sampled.end()) {
-            es.insert(e);
+            if (es.find(e) == es.end()) {
+                es[e] = 0;
+            }
+            ++es[e];
         } 
     }
+    */
 }
 
-void edge_sampler(VSet& vs, ESet& es, const Edges& edges, float percent) {
+void edge_sampler(VCount& vs, ECount& es, const Edges& edges, float percent) {
     int n = edges.size();
     int n_sample = int(n * percent);
     unordered_set<int> res;
     while (res.size() < n_sample) {
-        // original graph is 1-indexed
         res.insert(rand() % n);
     }
     for (int i: res) {
         auto& e = edges[i];
-        es.insert(e);
-        vs.insert(e.first);
+        if (es.find(e) == es.end()) {
+            es[e] = 0;
+        }
+        if (vs.find(e.first) == vs.end()) {
+            vs[e.first] = 0;
+        }
+        ++vs[e.first];
+        ++es[e];
     }
 }
 
 int main(int argc, const char** argv) {
     string method = "uniform";
     float percent = 0.01;
-    if (argc > 1) {
-        if (argc == 3) {
-            method = argv[1];
-            percent = stof(argv[2]);
-        }
+    int nsamples = 0;
+    if (argc == 4) {
+        method = argv[1];
+        percent = stof(argv[2]);
+        nsamples = stoi(argv[3]);
     } else {
         printf("error usage\n");
         exit(-1);
@@ -144,16 +157,17 @@ int main(int argc, const char** argv) {
     }
 
     n_e = edges.size();
-    VSet vs;
-    ESet es;
-    sprintf(buf, "tests/%s_%.2f.txt", method.c_str(), percent);
-    fp = fopen(buf, "w");
-    fprintf(fp, "%d\t%d\n", n_v, n_e);
-    fclose(fp);
-    int n_vs = 0;
-    int n_es = 0;
+    VCount vs;
+    ECount es;
+    sprintf(buf, "tests/%s_%.2f_shuffle_stats.txt", method.c_str(), percent);
+    //fp = fopen(buf, "w");
+    //fprintf(fp, "%d\t%d\n", n_v, n_e);
+    //fclose(fp);
+    //int n_vs = 0;
+    //int n_es = 0;
     int count = 0;
-    while (n_vs < n_v * 0.99 || n_es < n_e * 0.99 || count < 100) {
+    int interval = nsamples / 10;
+    while (count < nsamples) {
         if (method == "uniform") {
             update_vertex(vs, es, edges, uniform_sampler(n_v, edges, percent));
         } else if (method == "bfs") {
@@ -164,14 +178,29 @@ int main(int argc, const char** argv) {
             printf("Unknown sampling method: %s\n", method.c_str());
             exit(-1);
         }
-        n_vs = vs.size();
-        n_es = es.size();
-        fp = fopen(buf, "a");
-        fprintf(fp, "%d\t%d\t%.2f\t%.6f\n", n_vs, n_es, (float)n_vs / n_v, (float)n_es / n_e);
-        fclose(fp);
+        //n_vs = vs.size();
+        //n_es = es.size();
+        //fp = fopen(buf, "a");
+        //fprintf(fp, "%d\t%d\t%.2f\t%.6f\n", n_vs, n_es, (float)n_vs / n_v, (float)n_es / n_e);
+        //fclose(fp);
         // printf("sample %d\tv: %d / %d %.2f\te: %d / %d %.6f\n", count, n_vs, n_v, (float)n_vs / n_v, n_es, n_e, (float)n_es / n_e);
         ++count;
+        if (count % interval == 0) {
+            printf("%d / %d\n", count, nsamples);
+        }
     }
+    
+    fp = fopen(buf, "w");
+    for (auto& it: vs) {
+        fprintf(fp, "%d\t%d\n", it.first, it.second);
+    }
+    fprintf(fp, "#\n");
+    /*
+    for (auto& it: es) {
+        fprintf(fp, "%d\t%d\t%d\n", it.first.first, it.first.second, it.second);
+    }
+    */
+    fclose(fp);
 
     return 0;
 }
