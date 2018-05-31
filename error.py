75909203285654a7e8b3ca08740d738a335de8d9
Lingfan Yu <ly979@nyu.edu>
2018-05-31 04:40:04 +0000

    options when showing error: largest cc, dangling vs non-dangling

--- error.py
+++ error.py
@@ -9,7 +9,7 @@ def error(pr_full, pr_sample):
 
 if __name__ == '__main__':
     import sys
-    f1 = "result_full.txt"
+    f1 = "result_LCC_full.txt"
     assert(len(sys.argv) > 1)
     f2 = sys.argv[1]
 
@@ -22,7 +22,32 @@ if __name__ == '__main__':
         pr_sample = map(float, list(f))
         print(len(pr_sample))
 
+    # load vertex degree
+    with open('vertex_degree.txt') as f:
+        degree = map(int, f)
+
+    vertex = np.array(range(1, len(degree) + 1))
+    degree = np.array(degree)
+    dangling = vertex[degree < 3]
+
+    """
+    # nodes in largest connected component
+    with open('largest_cc.txt') as f:
+        nodes = map(int, f)
+        nodes = np.array(nodes)
+    """
+
+    # dangling nodes in largest CC
+    #nodes = np.intersect1d(dangling, nodes)
+
+    # non-dangling nodes in largest CC
+    #nodes = np.setdiff1d(nodes, dangling)
+
+    # convert to 0-index
+    #nodes = nodes - 1
+
     delta = error(pr_full, pr_sample)
+    #delta = delta[nodes]
 
     delta = np.sort(delta)
     n = len(delta)
@@ -30,5 +55,6 @@ if __name__ == '__main__':
     print(delta[int(n*0.1)])
     print(delta[int(n*0.2)])
     print(delta[int(n*0.5)])
+    print(delta[int(n*0.8)])
     print(delta[int(n*0.9)])
     print(delta[int(n*0.99)])
